71c5614a6e8f87415536bf67abc9a1a32857d101
Patrick-Njiru <patricknjiru93@gmail.com>
2023-04-10 10:40:18 +0300

    modify templates and views.

--- employee/views.py
+++ employee/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponseRedirect
 from django.urls import reverse
+from django.contrib import messages
 from employee.models import Employee
 from employee.forms import EmployeeForm
 
@@ -19,24 +20,27 @@ def create(request):
             try:
                 form.save()
             except:
-                pass
-        return HttpResponseRedirect(reverse("employees:index"))
+                # fix error display
+                return render(request, 'employee/create.html', {"error": "Error Encountered"})
+            return HttpResponseRedirect(reverse("employees:index"))
     else:
         form = EmployeeForm()
     return render(request, 'employee/create.html', {'form': form})
 
 def update(request, id):
     employee = get_object_or_404(Employee,id=id)
-    form = EmployeeForm(request.POST,  instance=employee)
-    if form.is_valid():
-        try:
-            form.save()
-        except:
-           pass
-        return HttpResponseRedirect(reverse("employees:show", args=(id,)))
+    if request.method == 'POST':
+        form = EmployeeForm(request.POST,  instance=employee)
+        if form.is_valid():
+            try:
+                form.save()
+            except:
+                # fix error display
+                return render(request, 'employee/update.html', {"error": "Error Encountered", 'employee': employee, 'form': form})
+            return HttpResponseRedirect(reverse("employees:show", args=(id,)))
     else:
         form = EmployeeForm(instance=employee)
-    return render(request, 'employee/update.html', {'employee': employee})
+    return render(request, 'employee/update.html', {'form': form, 'employee': employee})
 
 def destroy(request, id):
     employee = get_object_or_404(Employee,id=id)
